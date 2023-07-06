d3560d3db10f54869b102bc2fa278d25dc471775
Olivia Edwards <olivia@auli.tech>
2023-07-06 11:08:16 -0700

    writeConfig broke could be local computer angry

--- aulitech_site/my-app/src/components/ConfigureCato/Configure.jsx
+++ aulitech_site/my-app/src/components/ConfigureCato/Configure.jsx
@@ -3,6 +3,7 @@ import { get, set, clear } from 'https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm'
 
 const ConfigureCato = () => {
   const [catoConnected, setCatoConnected] = useState(false);
+  const [configSuccess, setConfigSuccess]  = useState(false);
   const [gestureNum, setGestureNum] = useState(0);
   const [prevGesture, setPrevGesture]  = useState('');
   
@@ -10,6 +11,7 @@ const ConfigureCato = () => {
     clear();
     setCatoConnected(false);
   }
+
 
   const getDirectory = async() => {
     try {
@@ -22,12 +24,14 @@ const ConfigureCato = () => {
       }
 
       const dirHandle = await window.showDirectoryPicker({
-        id: 'AULI_CATO'
+        id: 'AULI_CATO',
+        mode: 'readwrite'
       });
 
       await set('directory', dirHandle);
       console.log('store dir handle:', dirHandle.name);
       setCatoConnected(true);
+      
     }
     catch(error) {
       console.log("get directory error:", error);
@@ -36,22 +40,30 @@ const ConfigureCato = () => {
 
 
   const writeConfig = async() => {
-    // const directory = await get('directory');
-    // console.log(directory);
+    //const directory = await get('directory');
     try {
       const directory = await get('directory');
       console.log(directory);
-      // if(typeof directory !== 'undefined') {
-        if((await directory.queryPermission()) === 'granted') {
+
+      if(typeof directory !== 'undefined') {
+        const perm = await directory.requestPermission()
+
+        if(perm === 'granted') {
           const configFile = await directory.getFileHandle('config.cato', { create: true });
           
           console.log('Config.cato:', configFile);
           
           const writable = await configFile.createWritable();
+          console.log(gestureNum);
           await writable.write(gestureNum);
           await writable.close();
-        }
-//      }
+
+          const checkConfig = await directory.getFileHandle('config.cato', { create: false })
+          if(checkConfig !== null) {
+            setConfigSuccess(true);
+          };
+        };
+      }
     }
     catch(error) {
       console.log("write config.cato error:", error);
@@ -93,8 +105,23 @@ const ConfigureCato = () => {
         </>
         :
         <>
-          <button onClick={getDirectory}>Grant Access</button>
+          <button onClick={getDirectory}>Connect</button>
         </>}
+      </div>
+    )
+  }
+
+  const HandleConfigUI = () => {
+    return (
+      <div>
+        <p>Hit start to begin recording gesture.</p>
+        <button onClick={writeConfig} disabled={configSuccess}>Start</button>
+        <br/>
+        {configSuccess ? 
+        <>   
+          <img src={require('../../images/check-icon.png')} alt='circle check icon' width={'3%'}/>  
+        </>
+        : null}
       </div>
     )
   }
@@ -115,19 +142,16 @@ const ConfigureCato = () => {
           <option value={1}>Nod up</option>
           <option value={2}>Nod down</option>
           <option value={3}>Nod right</option>
-          <option value={4}>Tilt right</option>
-          <option value={5}>Tilt left</option>
-          <option value={6}>Shake vertical</option>
-          <option value={7}>Shake horizontal</option>
-          <option value={8}>Circle clockwise</option>
-          <option value={9}>Circle counterclockwise</option>
+          <option value={4}>Nod left</option>
+          <option value={5}>Tilt right</option>
+          <option value={6}>Tilt left</option>
+          <option value={7}>Shake vertical</option>
+          <option value={8}>Shake horizontal</option>
+          <option value={9}>Circle clockwise</option>
+          <option value={10}>Circle counterclockwise</option>
         </select>
       </div>
-      <div>
-        <p>Map Gesture</p>
-        <button onClick={writeConfig}>Start</button>
-      </div>
-      <br/>
+      <HandleConfigUI/>
     </>
   )
 }
