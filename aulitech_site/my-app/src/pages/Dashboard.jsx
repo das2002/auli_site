9f58f7296a4c62419fa97dc8d71209052a6f6109
Olivia Edwards <olivia@auli.tech>
2023-07-12 18:48:53 -0700

    create device access status panel on dash

--- aulitech_site/my-app/src/pages/Dashboard.jsx
+++ aulitech_site/my-app/src/pages/Dashboard.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { clear } from 'idb-keyval';
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-import ConnectDirectory from "../components/ConfigureCato/ConnectDirectory";
+import { db } from "../firebase";
+import ConnectDirectory from "../components/DeviceConnection/ConnectDirectory";
+import WriteAccess from "../components/DeviceConnection/WriteAccess";
+import GestDataAccess from "../components/DeviceConnection/GestDataAccess";
 
-const Dashboard = ({classNames}) => {
-  
+const Dashboard = ({classNames, user}) => {
+  const [data, setData] = useState(null)
+
+  const getUserData = async() => {
+    try{
+      const userQuery = query(collection(db, "users"), where("email", "==", user.email))
+
+      const querySnapshot = await getDocs(userQuery);
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        setData(doc.data());
+        });
+    }
+    catch(error) {
+      console.log("query user collection error:", error);
+    }
+
+  }
+  getUserData();
+
   const reset = () => {
     clear();
-  }
-
-  const handleCatoConnected = (connectState) => {
-    console.log(connectState);
   }
 
   return (
@@ -25,11 +43,13 @@ const Dashboard = ({classNames}) => {
             <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">{/* Main area */} hi</div>
           </div>
 
-          <div className="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6">
+          <div className="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6 ">
             <div className="border-b border-gray-200 pb-5">
-              <h3 className="text-base font-semibold leading-6 text-gray-900">Device Connection Status</h3>
+              <h3 className="text-base font-semibold leading-6 text-gray-900">Device Access Status</h3>
             </div>
-            <ConnectDirectory classNames={classNames} reset={reset} handleCatoConnected={handleCatoConnected}/>
+            <ConnectDirectory classNames={classNames}/>
+            <WriteAccess classNames={classNames}/>
+            <GestDataAccess classNames={classNames}/>
             <button
               type="button"
               onClick={reset}
