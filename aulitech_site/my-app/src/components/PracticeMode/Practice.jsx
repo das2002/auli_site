22f8609ac6a907da2cafa7647bbb7d7edcdde07e
Pratyush Das <prat.das2002@gmail.com>
2024-01-02 20:28:05 -0800

    practice mode changes

--- aulitech_site/my-app/src/components/PracticeMode/Practice.jsx
+++ aulitech_site/my-app/src/components/PracticeMode/Practice.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const CompatibilityCheck = ({isCompatible}) => {
     if (isCompatible) {
@@ -7,19 +8,97 @@ const CompatibilityCheck = ({isCompatible}) => {
     return <p>Your browser does not support Bluetooth. Please use a compatible browser.</p>;
 };
 
+let originalMode = '';
+let originalJson = null;
+    
+const deepCopy = (obj) => {
+  return JSON.parse(JSON.stringify(obj));
+};
+
+
 const ConnectionInstructions = ({ onNext }) => {
+    //const [currentMode, setCurrentMode] = useState(''); // [currentMode, setCurrentMode
+    const [parsedJson, setParsedJson] = useState({}); // [parsedJson, setParsedJson
+    const [previousMode, setPreviousMode] = useState('');
+
+    useEffect(() => {
+      originalMode = deepCopy(previousMode);
+    }, [previousMode]);
+
+    useEffect(() => {
+      originalJson = deepCopy(parsedJson);
+      console.log('originalJson', originalJson);
+    }, [parsedJson]);
+    const getJsonData = async () => {
+        try {
+            if (window.showDirectoryPicker) {
+                try {
+                    // request the user to pick a directory
+                    const dirHandle = await window.showDirectoryPicker({
+                        id: "AULI_CATO",
+                        mode: "readwrite",
+                    });
+                    // attempt to find the config.json file in the directory
+                    for await (const entry of dirHandle.values()) {
+                        if (entry.kind === "file" && entry.name === "config.json") {
+                            // found the file, read it
+                            const file = await entry.getFile();
+                            const jsonDataText = await file.text();
+                            let parsedJson = JSON.parse(jsonDataText);
+                            let globalConfig = deepCopy(parsedJson);
+                            setParsedJson(parsedJson);
+                            console.log(parsedJson);
+                            setPreviousMode(parsedJson['operation_mode']['value']);
+                            globalConfig['operation_mode']['value'] = 'practice';
+
+
+                            // create a practice mode file
+                            const fileName = "config.json";
+                            const fileData = JSON.stringify(globalConfig, null, 2);
+                            const newFileHandle = await dirHandle.getFileHandle(fileName, { create: true });
+                            const writable = await newFileHandle.createWritable();
+                            await writable.write(fileData);
+                            await writable.close();
+
+                            const blob = new Blob([fileData], { type: 'application/json' });
+                            const url = URL.createObjectURL(blob);
+                            const link = document.createElement('a');
+                            link.href = url;
+                            link.download = fileName;
+
+                            // this is a hack to fix the issue with downloading the file
+                            link.click();
+
+                            link.remove();
+
+                            return;
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                console.log("window.showDirectoryPicker is not supported");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
       <div style={styles.container}>
         <h2 style={styles.heading}>Instructions to Put Device in Practice Mode:</h2>
         <ol style={styles.list}>
           <li>Connect your Cato to the computer via USB.</li>
-          <li>Access the file system and find the device under your USB devices.</li>
-          <li>Find <strong>config.json</strong>.</li>
-          <li>Open up <strong>config.json</strong> and change the <strong>label</strong> field in operation mode to 'practice'.</li>
-          <li>Save the file.</li>
+          <li>Click on the <strong>Activate Practice Mode</strong> and allow permission to access your Cato device.</li>
+          <li>Replace the existing <strong>config.json</strong> with the downloaded file.</li>
           <li>Properly eject the device from USB connection.</li>
           <li>Connect to the device via Bluetooth.</li>
+          <li>Click on the 'Ready' button to start practice mode.</li>
         </ol>
+        <button onClick={getJsonData} style={styles.button}>Activate Practice Mode</button>
+        <div style={{ margin: '10px' }}></div>
         <button onClick={onNext} style={styles.button}>Ready</button>
       </div>
     );
@@ -71,12 +150,25 @@ const BluetoothCheck = ({onCheck}) => {
 };
 
 const PracticeMode = () => {
+    const Navigate = useNavigate();
     const [isPracticing, setIsPracticing] = useState(false);
     const [practiceText, setPracticeText] = useState('');
+    const [originalMode, setOriginalMode] = useState('');
     const textareaRef = useRef(null);
     const togglePractice = () => {
         if (isPracticing) {
             textareaRef.current.blur();
+            // download the original config.json file
+            const fileName = "config.json";
+            const fileData = JSON.stringify(originalJson, null, 2);
+            const blob = new Blob([fileData], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = fileName;
+            link.click();
+            link.remove();
+            Navigate("/")
         } else {
             textareaRef.current.focus();
         }
