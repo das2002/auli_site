cd0c8e108bea976dc4fa3d1305a1ad579de935d1
Emily Park <86273173+mlapark@users.noreply.github.com>
2024-02-13 17:22:42 -0800

    var name fix

--- aulitech_site/my-app/src/components/NavBar/Navigation.jsx
+++ aulitech_site/my-app/src/components/NavBar/Navigation.jsx
@@ -161,7 +161,7 @@ const DevicesList = React.memo(({ onClick }) => {
                         fill="currentColor"
                         className="w-6 h-6 ml-2"
                       >
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 102 0V6zm-1 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 102 0V6zm-1 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
                       </svg>
                     </div>
                   )}
